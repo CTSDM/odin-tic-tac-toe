b505b34697a532224fc009a5626a9d652e23f4c9
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-03-11 15:55:52 +0100

    Add logic and style for the winning tiles

--- script.js
+++ script.js
@@ -38,23 +38,23 @@
         }
 
         const checkVictory = function(x, y) {
+            // horizontal checking
             if ((arr[x][0] === arr[x][1]) && (arr[x][1] === arr[x][2]))
-                return true;
+                return {victory: true, state: 0};
+            // vertical checking
             if ((arr[0][y] === arr[1][y]) && (arr[1][y] === arr[2][y]))
-                return true;
+                return {victory:true, state: 1};
+            // check lef-to-right downward diagonal
             if(x === y) {
-                // check lef-to-right downward diagonal
                 if ((arr[0][0] === arr[1][1]) && (arr[1][1] === arr[2][2]))
-                    return true;
-                if (y === 1) {
-                    if ((arr[0][2] === arr[1][1]) && (arr[1][1] === arr[2][0]))
-                        return true;
-                }
+                    return {victory: true, state: 2};
             } 
+            // check right-to-left downward diagonal
             if ((x === 2 && y === 0) || (x === 0 && y === 2)) {
                 if ((arr[2][0] === arr[1][1]) && (arr[1][1] === arr[0][2]))
-                    return true;
+                    return {victory: true, state: 4};
             }
+            return {victory: false, state: undefined};
         }
 
         return {updateBoard, resetBoard, getMoves};
@@ -77,14 +77,14 @@
             const index = rowIndex * 3 + columnIndex;
             itemsNotUsed.splice(index, 1);
             itemsUsed.push(index);
-            victory = gameBoard.updateBoard(rowIndex, columnIndex, arrPlayers[turn_player]);
+            ({victory, state} = gameBoard.updateBoard(rowIndex, columnIndex, arrPlayers[turn_player]));
             item.appendChild(imgEl);
             if (victory) {
                 arrPlayers[turn_player].increaseScore();
                 updateScoreDOM();
                 // we remove the event listener from where it hasn't been used
                 itemsNotUsed.forEach((idx) => itemsNodeList[idx].removeEventListener("click", playOcurrence));
-                
+                showWinnerDOM(state, rowIndex, columnIndex);
                 createButtons();
             } else if (gameBoard.getMoves() === 9) {
                 createButtons();
@@ -107,6 +107,31 @@
         buttonContainer.appendChild(buttContinue);
     }
 
+
+    // in this function we add a class to the positions that allowed winning
+    function showWinnerDOM(state, rowIndex, columnIndex) {
+    const rows = document.querySelectorAll(".row");
+    if (state === 1) {
+        // column victory 
+        rows.forEach((row) => {
+            row.querySelectorAll(".item")[columnIndex].classList.add("winner");
+        })
+    } else if (state === 0) {
+        const items = rows[rowIndex].querySelectorAll(".item");
+        for (const item of items) {
+            item.classList.add("winner");
+        }
+    } else if (state === 2) {
+        // downward diagonal from left-to-right
+        for (let i = 0; i < 3; ++i) {
+            rows[i].querySelectorAll(".item")[i].classList.add("winner");
+        }
+    } else {
+        for (let i = 0; i < 3; ++i) {
+            rows[2 - i].querySelectorAll(".item")[i].classList.add("winner");
+        }
+    }
+}
     function updateScoreDOM() {
         const scoreSpan = document.querySelector(`#score-${turn_player}`);
         scoreSpan.textContent = arrPlayers[turn_player].getScore();
@@ -129,7 +154,15 @@
         gameBoard.resetBoard();
         resetDisplay();
         itemsNodeList.forEach((item) => item.addEventListener("click", playOcurrence))
-        victory = false;
+        if (victory) {
+            victory = false;
+            const rows = document.querySelectorAll(".row");
+            for (const items of rows) {
+                for (const item of items.querySelectorAll(".item")) {
+                    item.classList.remove("winner");
+                }
+            }
+        }
         turn_player = (turn_player === 1) ? 0 : 1;
         const buttonsStates = document.querySelectorAll("button");
         buttonsStates.forEach((button) => button.remove());
@@ -146,7 +179,8 @@
     const p1 = createPlayer("X");
     const p2 = createPlayer("0");
     const arrPlayers = [p1, p2];
-    let victory = false;
+    // the line below needs a function to initialize/reset the state
+    let {victory, state}  = {victory: false, state: undefined};
     let turn_player = 0;
     gameBoard.resetBoard();
 })()
